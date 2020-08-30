2c5b9ae28dbada3b5ad3a04b8aef792c232a4530
wu-rymd <wu.raymond@columbia.edu>
2020-08-30 16:16:24 -0400

    push build 2nd term projects

---
+++ precache-manifest.c4c89557aafff217193a5cf17c99c365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d887cf6d4d7b430f72bf0f7a8247aae8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee859a15cfac6d375ee4",
+    "url": "/static/css/main.22826d95.chunk.css"
+  },
+  {
+    "revision": "5540c52528d175cf4ed5",
+    "url": "/static/js/2.2c2fbbd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c2fbbd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee859a15cfac6d375ee4",
+    "url": "/static/js/main.23742b15.chunk.js"
+  },
+  {
+    "revision": "0c0c47ce73a500bf802d",
+    "url": "/static/js/runtime-main.42b6a6bd.js"
+  }
+]);
